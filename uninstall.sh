3d6a23c2cbd6a4d7c13cddf8c2c9295485edd608
ivanhao <ivanhao1984@qq.com>
2020-08-17 23:05:04 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,9 +3,9 @@
 echo "uninstall pve-folder2ram? (y\n)"
 read x
 if [ $x == 'y' ];then
-   folder2ram -disablesystemd
    folder2ram -syncall
    folder2ram -umountall
+   folder2ram -disablesystemd
    
    sed -i '/truncLog/d' /etc/crontab
    systemctl restart cron
