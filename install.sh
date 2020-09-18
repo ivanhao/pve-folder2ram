a46b40fd995b371ca9138ef11048d36a6d1b9e1b
ivanhao <ivanhao1984@qq.com>
2020-09-18 09:07:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,6 +48,7 @@ EOF
 
     chmod +x /usr/bin/truncLog
     echo "" >> /etc/crontab
+    sed -i "/truncLog/d" /etc/crontab
     echo "@reboot */10  *	*	*	*	root	/usr/bin/truncLog" >> /etc/crontab
     systemctl restart cron
     echo "done."
